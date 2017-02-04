35d0e76e0670e9fab38e6c44db6c93c20776700b
craft <yooobuntu@163.com>
2017-02-04 14:41:43 +0800

    fix

--- two_sum#1/solution_1.rb
+++ two_sum#1/solution_1.rb
@@ -2,13 +2,10 @@
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(nums, target)
-	[].tap do |result|
-      hash = {}
-      nums.each_with_index do |n, i|
-        if j = hash[target - n]
-          return result << j + 1 << i + 1
-        end
-        hash[n] = i
-      end
-    end
+  res, hash = nil, {}
+  nums.each_with_index do |num, index|
+    res ||= [hash[num], index] if hash[num]
+    hash[target-num] = index
+  end
+  res
 end
